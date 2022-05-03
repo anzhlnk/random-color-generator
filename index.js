d6c9aab411c2588228aa8d0d0e7b02267a8829f3
Hanna <anzhelenyuk@gmail.com>
2022-05-03 22:52:42 +0200

    Remove errors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function drawHashRectangle(color) {
 ###############################
   `;
 }
-//let numOfRows = 9;
+// let numOfRows = 9;
 // let numOfColumns = 31;
 // let hashRectangle = '';
 // for (let i = 1; i <= numOfRows; i++) {
@@ -25,19 +25,19 @@ function drawHashRectangle(color) {
 
 if (process.argv.length === 4) {
   const lastIndex = process.argv.splice(2, 2);
-  let myColor = randomColor({ luminosity: lastIndex[0], hue: lastIndex[1] });
-  let blackAndWhiteHashRectangle = drawHashRectangle(myColor); // output string with hex color code, but still black and white
-  let coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
+  const myColor = randomColor({ luminosity: lastIndex[0], hue: lastIndex[1] });
+  const blackAndWhiteHashRectangle = drawHashRectangle(myColor); // output string with hex color code, but still black and white
+  const coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
   console.log(coloredHashRectangle);
 } else if (process.argv.length === 3) {
-  let lastIndex = process.argv.splice(2, 1);
-  let myColor = randomColor({ hue: lastIndex[0] });
-  let blackAndWhiteHashRectangle = drawHashRectangle(myColor);
-  let coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
+  const lastIndex = process.argv.splice(2, 1);
+  const myColor = randomColor({ hue: lastIndex[0] });
+  const blackAndWhiteHashRectangle = drawHashRectangle(myColor);
+  const coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
   console.log(coloredHashRectangle);
 } else {
-  let myColor = randomColor();
-  let blackAndWhiteHashRectangle = drawHashRectangle(myColor);
-  let coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
+  const myColor = randomColor();
+  const blackAndWhiteHashRectangle = drawHashRectangle(myColor);
+  const coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
   console.log(coloredHashRectangle);
 }
