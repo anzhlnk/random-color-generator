f9433dc84f01bb0369e52be72e4d5685d7a0d01e
Hanna <anzhelenyuk@gmail.com>
2022-05-03 17:37:45 +0200

    Create second version

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-let numOfRows = 9;
-let numOfColumns = 31;
-let hashRectangle = '';
-for (let i = 1; i <= numOfRows; i++) {
-  for (let j = 1; j <= numOfColumns; j++) {
-    hashRectangle += '#';
-  }
-  hashRectangle += '\n';
-}
+let myColor = randomColor();
+let colorAdapted = chalk.hex(myColor);
 
-const colorAdapted = chalk.hex(randomColor());
+let hashRectangle = `###############################
+###############################
+###############################
+####                       ####
+####       ${myColor}         ####
+####                       ####
+###############################
+###############################
+###############################
+`;
+
+//let numOfRows = 9;
+// let numOfColumns = 31;
+// let hashRectangle = '';
+// for (let i = 1; i <= numOfRows; i++) {
+//   for (let j = 1; j <= numOfColumns; j++) {
+//     hashRectangle += '#';
+//   }
+//   hashRectangle += '\n';
+// }
 
 if (process.argv.length === 4) {
   const lastIndex = process.argv.splice(2, 2);
   console.log(lastIndex);
-  let colorChosen = chalk.hex(
-    randomColor({ luminosity: lastIndex[0], hue: lastIndex[1] }),
+  colorAdapted = chalk.hex(
+    (myColor = randomColor({ luminosity: lastIndex[0], hue: lastIndex[1] })),
   );
-  console.log(colorChosen(hashRectangle));
+  console.log(colorAdapted(hashRectangle));
 } else if (process.argv.length === 3) {
   let lastIndex = process.argv.splice(2, 1);
   console.log(lastIndex);
-  const colorChosen = chalk.hex(randomColor({ hue: lastIndex[0] }));
-  console.log(colorChosen(hashRectangle));
+  colorAdapted = chalk.hex((myColor = randomColor({ hue: lastIndex[0] })));
+  console.log(colorAdapted(hashRectangle));
 } else {
   console.log(colorAdapted(hashRectangle));
 }
-console.log(hashRectangle.length);
