f7e262a4925078fdd6b7b3e4d8b1505da79df5ee
Hanna <anzhelenyuk@gmail.com>
2022-05-04 20:03:11 +0200

    Fix the order of hue and luminosity

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ $ node index.js
 
 ![random-color-generator](random-color.jpg)
 
-When a user enters luminosity (e.g. light or dark) and hue (e.g. green or red or blue), or hue only, the program generates random colors that match those choices:
+When a user enters hue (e.g. green or red or blue) and luminosity (e.g. light or dark), or hue only, the program generates random colors that match those choices:
 
 ```bash
-$ node index.js light red
+$ node index.js red light
 ```
 
-![random-color-generator](light-red.jpg)
+![random-color-generator](red-light.jpg)
 
 ```bash
 $ node index.js blue
--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function drawHashRectangle(color) {
   return `###############################
 ###############################
 ###############################
-####                       ####
-####       ${color}         ####
-####                       ####
+#####                     #####
+#####       ${color}       #####
+#####                     #####
 ###############################
 ###############################
 ###############################
@@ -25,7 +25,7 @@ function drawHashRectangle(color) {
 
 if (process.argv.length === 4) {
   const lastIndex = process.argv.splice(2, 2);
-  const myColor = randomColor({ luminosity: lastIndex[0], hue: lastIndex[1] });
+  const myColor = randomColor({ luminosity: lastIndex[1], hue: lastIndex[0] });
   const blackAndWhiteHashRectangle = drawHashRectangle(myColor); // output string with hex color code, but still black and white
   const coloredHashRectangle = chalk.hex(myColor)(blackAndWhiteHashRectangle);
   console.log(coloredHashRectangle);
